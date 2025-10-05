5f2979e8e1c274f07129e8f3b33ed906bb5bf5ef
kau4dev <kauavictordev7@gmail.com>
2025-10-05 16:55:16 -0300

    refactor: simplifica o esquema de criação de serviços removendo a estrutura desnecessária do corpo

--- src/servicos/servicoSchema.ts
+++ src/servicos/servicoSchema.ts
@@ -1,19 +1,17 @@
 import { z } from "zod";
 
 export const createServicoSchema = z.object({
-  body: z.object({
-    nome: z
-      .string()
-      .min(3, { message: "O nome precisa ter no mínimo 3 caracteres" }),
-    preco: z
-      .number()
-      .positive({ message: "O preço deve ser um número positivo" }),
-    duracao: z
-      .number()
-      .int({ message: "A duração deve ser um número inteiro" })
-      .positive({ message: "A duração deve ser um número positivo" }),
-    descricao: z.string().optional(),
-  }),
+  nome: z
+    .string()
+    .min(3, { message: "O nome precisa ter no mínimo 3 caracteres" }),
+  preco: z
+    .number()
+    .positive({ message: "O preço deve ser um número positivo" }),
+  duracao: z
+    .number()
+    .int({ message: "A duração deve ser um número inteiro" })
+    .positive({ message: "A duração deve ser um número positivo" }),
+  descricao: z.string().optional(),
 });
 
 export const updateServicoSchema = z.object({
